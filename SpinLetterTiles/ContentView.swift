ec29fc7e8f9dab4917ab5ad595c8ac9e0cdfd8d7
Mike Lewis <revmikelewis@gmail.com>
2025-11-11 10:13:29 -0600

    Initial Commit

---
+++ SpinLetterTiles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpinLetterTiles
+//
+//  Created by Mike Lewis on 11/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpinLetterTiles/SpinLetterTilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinLetterTilesApp.swift
+//  SpinLetterTiles
+//
+//  Created by Mike Lewis on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SpinLetterTilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
